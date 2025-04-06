314a49faca638da4f174ba22f82ac80741facd5b
Steve <106431302+kstevejose@users.noreply.github.com>
2025-04-06 15:07:41 +0530

    edited orgName and projectName

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -18,8 +18,8 @@ const config: Config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'kstevejose', // Usually your GitHub org/user name.
+  projectName: 'API_Documentation', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
